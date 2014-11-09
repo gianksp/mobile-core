864cf0cce502a133fee519da5de3901f68d21aa0
Giancarlo <giancarlo@zappy.ie>
2014-11-09 20:53:53 +0000

    Latest

---
+++ Source/Assets/Scripts/Player.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+var code;
+
+function Start () {
+//	rigidbody.isKinematic = true;
+//    rigidbody.detectCollisions = false;
+}
+
+function Update () {
+	code = PlayerPrefs.GetString("code");
+	if (code != null) {
+		eval(code);
+	}
+
+}
+
+function OnGUI () {
+	code = PlayerPrefs.GetString("code");
+	// Make a multiline text area that modifies stringToEdit.
+	code = GUI.TextArea (Rect (10, 60, 250, 800), code, 1000);
+	PlayerPrefs.SetString("code",code);
+//	
+//	if (GUI.Button(Rect(Screen.width - 150, 10 , 100 ,30),"Click")) {
+//        
+//        rigidbody.isKinematic = false;
+//        rigidbody.detectCollisions = true;
+//	}
+
+}
